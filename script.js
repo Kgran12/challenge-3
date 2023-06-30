63ddc95535a5eab53afc66239fff30f3ed77b487
Kyle Gran <kgran@gmail.com>
2023-06-30 16:07:05 -0700

    created initial files and established variables

---
+++ script.js
@@ -0,0 +1,20 @@
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = "128"
+
+let prompt
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
